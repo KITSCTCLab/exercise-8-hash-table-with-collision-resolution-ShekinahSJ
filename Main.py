57b364219a4307ac4264db54202578475702d167
ShekinahSJ <100009660+ShekinahSJ@users.noreply.github.com>
2022-09-19 20:44:50 +0530

    Fifth Commit

--- Main.py
+++ Main.py
@@ -3,11 +3,10 @@ import re
 def display_hash(hashtable) -> None:
 	# Write your code here
 	for i in range(len(hashtable)):
-        print(i, end = " ")
-          
-        for j in hashtable[i]:
-            print("-->", end = " ")
-            print(j, end = " ")
+		print(i, end = " ")
+		for j in hashtable[i]:
+			print("-->", end = " ")
+		    	print(j, end = " ")
               
         print()
 
